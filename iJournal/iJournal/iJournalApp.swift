0a3d1fedfcfb23868d1ba03918b77643a7bb0c29
linuxmaster0312 <59486852+linuxmaster0312@users.noreply.github.com>
2024-02-14 12:16:30 -0500

    initial commmit. fresh xcode project for MacOS app

---
+++ iJournal/iJournal/iJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iJournalApp.swift
+//  iJournal
+//
+//  Created by Josh Park on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct iJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
